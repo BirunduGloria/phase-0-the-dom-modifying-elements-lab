0593f05340bd9164c585e270602c3ecb766ae1e1
Gloria Birundu <birundugloria@gmail.com>
2025-06-29 19:18:32 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Remove the <main id="main"> element from the DOM
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Assign it the ID of 'victory'
+newHeader.id = "victory";
+
+// Set the text inside the newHeader
+newHeader.textContent = "GLORIA is the champion";
+
+// Add the newHeader to the document body
+document.body.append(newHeader);
